6223e370382249f5d48ef289b2ccf272d9dec995
taitz <taitzariel@gmail.com>
2017-12-21 22:28:59 +0200

    spring initializer https://start.spring.io

---
+++ src/main/java/com/taitz/housepicker/HousePickerApplication.java
@@ -0,0 +1,12 @@
+package com.taitz.housepicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousePickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousePickerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/taitz/housepicker/HousePickerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.taitz.housepicker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HousePickerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
