a106a16bd00da588c02bf2e5f77bd2719ede658f
taitz <taitzariel@gmail.com>
2017-12-21 22:55:34 +0200

    rest controller

--- src/main/java/com/taitz/housepicker/HousePickerApplication.java
+++ src/main/java/com/taitz/housepicker/HousePickerApplication.java
@@ -2,11 +2,19 @@ package com.taitz.housepicker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class HousePickerApplication {
 
-	public static void main(String[] args) {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(HousePickerApplication.class, args);
 	}
 }
